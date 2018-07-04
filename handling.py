73a31f91c078830c3e221cc32904c774c4985674
vinay-yadav <1996vinayofficial@gmail.com>
2018-07-04 10:51:21 +0530

    Error hadling basic code

---
+++ handling.py
@@ -0,0 +1,21 @@
+try:
+    a = 9
+    #b = a/0                             #Zero division error
+    f = open("abc.txt")                 #File not found
+    for line in f:
+        print(line)
+
+# except FileNotFoundError:
+#     print("File not find")
+#
+# except ZeroDivisionError:
+#     print("Divided by zero you moron")
+
+# except (FileNotFoundError, ZeroDivisionError):        #combined excetion
+#      print("error caught")
+
+# except Exception as e:                      #accepts all kinds of exception
+#     print("Error : ",e)
+
+except FileNotFoundError as e:
+    print(e.filename)
